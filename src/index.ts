01246623fac6af9af450991d8dba1d2730a607d7
Kevin Wynn <kevinwynn@Kevins-MacBook-Pro.local>
2025-05-29 20:39:37 -0500

    initial work

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,48 @@
 export default {
-	async fetch(request: Request): Promise<Response> {
-		/**
-		 * Replace `remote` with the host you wish to send requests to
-		 */
-		const remote = "https://example.com";
+  async fetch(request: Request): Promise<Response> {
+    const { searchParams } = new URL(request.url);
+    const url = searchParams.get("url");
+    if (!url) {
+      if (request.url.includes("favicon.ico")) {
+        return new Response(null, {
+          status: 200,
+          headers: {
+            "Content-Type": "image/x-icon",
+          },
+        });
+      }
 
-		return await fetch(remote, request);
-	},
+      return new Response("No URL provided", {
+        status: 400,
+        headers: {
+          "Content-Type": "text/plain",
+        },
+      });
+    }
+
+    const res = await fetch(url ?? "", { ...request, redirect: "manual" });
+
+    let text = await res.text();
+
+    let returnText = updateLinksInHTML(text, url ?? "");
+
+    return new Response(returnText, {
+      status: res.status === 301 || res.status === 302 ? 200 : res.status,
+      statusText: res.statusText,
+      headers: res.headers,
+    });
+  },
+};
+
+const updateLinksInHTML = (html: string, url: string): string => {
+  const baseUrl = `https://linksie-poc.robo-house.workers.dev/?url=${url}`;
+  html = html.replace(/href="(.*?)"/g, (_m: any, v: string) => {
+    return 'href="' + baseUrl + v + '"';
+  });
+
+  html = html.replace(/src="(.*?)"/g, (_m: any, v: string) => {
+    return 'src="' + baseUrl + v + '"';
+  });
+
+  return html;
 };
